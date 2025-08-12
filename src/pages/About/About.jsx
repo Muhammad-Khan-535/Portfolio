21fc44d96302dfe3815454ef29a8bdd2c4459934
Muhammad Khan <muhammadkhanchanna535@gmail.com>
2025-08-12 14:11:01 +0500

    Portfolio

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,57 @@
+import HeroImg from "@/assets/images/hero.jpg";
+
+export default function About() {
+  return (
+    <>
+      <section id="about" className="py-16 md:py-32  text-white bg-[#04081A]">
+        <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
+          <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl text-white">
+            Developer, Designer, Creator
+          </h2>
+          <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
+            <div className="relative mb-6 sm:mb-0">
+              <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl p-px from-zinc-300 to-transparent">
+                <img
+                  src={HeroImg}
+                  className="rounded-[15px] shadow block"
+                  alt="payments illustration"
+                  width={1207}
+                  height={929}
+                />
+              </div>
+            </div>
+
+            <div className="relative space-y-4">
+              <p className="text-white">
+                Hi, I’m Muhammad Khan. A passionate JavaScript developer
+                specializing in creating innovative web solutions and
+                user-friendly interfaces. I focus on making web development
+                faster, easier, and more accessible for everyone. I’m a Frontend
+                Developer and a final-year Computer Systems Engineering student
+                at Mehran University of Engineering and Technology, Jamshoro,
+                where I’m honing my expertise in modern web technologies.
+              </p>
+              <p className="text-white">
+                My focus is on making web development faster, easier, and
+                accessible to all developers. Currently, I'm expanding into
+                backend development to grow as a full-stack developer and create
+                seamless, robust web applications.
+              </p>
+
+              <div className="pt-6">
+                <blockquote className="border-l-4 border-gray-300 pl-4">
+                  <p className="text-white">
+                    I'm a lifelong learner and innovator, driven by a desire to
+                    contribute to the developer community with new ideas and
+                    tools that deliver real value. I'm pushing the boundaries of
+                    JavaScript frameworks to empower developers worldwide.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
